6e8f8822837bf0acb29062098b5032b9d381f279
teamzz111 <aflr@outlook.com>
2018-10-19 23:13:57 -0500

    Proyecto Ingeniería de Software.
    Versión preliminar web.

---
+++ gui/gui/php/Conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+  global $host, $db, $pass, $key, $user;
+  $host = "sql100.hostlibre.ml";
+  //$host = "localhost";
+  $db = "teolo_21117533_sgc";
+  $pass = "3192934192321";
+  $user = "teolo_21117533";
+  $key = "92AE31B89FEEB2A3"; //llave
+
+  
+  $db2 = "teolo_21117533_encuestas";
+  $pass2 = "3192934192321";
+  $user2 = "teolo_21117533";
+
+ ?>
